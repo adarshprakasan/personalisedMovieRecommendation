2183a96f6f6aa80946dcfbef0ac7121402aadea8
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2025-01-28 00:23:07 +0530

    code correcctions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.routers import recommend
+from fastapi.middleware.cors import CORSMiddleware
+
+# app = FastAPI(
+#     title="Personalized Movie Recommendation System",
+#     version="1.0.0",
+#     description="A system that recommends movies based on user preferences."
+# )
 
 app = FastAPI()
 
@@ -7,6 +14,18 @@ origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173", 
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      
+    allow_credentials=True,        
+    allow_methods=["*"],           
+    allow_headers=["*"],            
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Movie Recommendation API!"}
 
 # Include the recommendation router
 app.include_router(recommend.router)
